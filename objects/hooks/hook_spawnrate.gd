538eda7124b12a04315d25f5b4ec2f04902e9ccd
_michdev <gamedev.mich@gmail.com>
2026-06-06 19:28:20 -0400

    started pooling

--- objects/hooks/hook_spawnrate.gd
+++ objects/hooks/hook_spawnrate.gd
@@ -8,3 +8,24 @@ class_name HookSpawnrate extends Resource
 @export_group("Logic")
 @export var scene: PackedScene
 @export var spawnrate: Curve
+@export_group("Pooling")
+@export var default_count: int
+
+func make_defaults() -> void:
+	for i in default_count:
+		make_hook(true)
+
+func get_hook() -> Hook:
+	if Hook.pool.get(self, []).is_empty():
+		return make_hook(false)
+	var pooled := (Hook.pool[self][0] as Hook).unstash()
+	return pooled if pooled else make_hook(false)
+
+func make_hook(stashed: bool) -> Hook:
+	var inst := scene.instantiate() as Hook
+	inst.spawnrate = self
+	if stashed:
+		inst.stash()
+	else:
+		World.world.add_child(inst)
+	return inst
